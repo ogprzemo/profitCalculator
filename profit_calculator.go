c4fe9ce833f24d63a35df87ca8c2e233835d53f7
Przemysław Brzuzy <96412768+ogprzemo@users.noreply.github.com>
2024-10-11 22:40:10 +0200

    update
    added handle errors and function that store our results in txt file.

--- profit_calculator.go
+++ profit_calculator.go
@@ -1,17 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	revenue := getUserInput("Enter revenue: ")
-	expenses := getUserInput("Enter expenses: ")
-	taxRate := getUserInput("Enter tax rate: ")
+	revenue, err1 := getUserInput("Enter revenue: ")
+	expenses, err2 := getUserInput("Enter expenses: ")
+	taxRate, err3 := getUserInput("Enter tax rate: ")
+
+	if err1 != nil || err2 != nil || err3 != nil {
+		fmt.Println(err1)
+		return
+	}
 
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
 	fmt.Printf("Electronic Benefits Transfer: %.2f\n", ebt)
 	fmt.Printf("Your profit: %.2f\n", profit)
 	fmt.Printf("Ratio: %.3f\n", ratio)
+	storeResults(ebt, ratio, profit)
+}
+
+func storeResults(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.2f\nProfit: %.3f\nRatio: %.2f\n", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(results), 0644)
 }
 
 func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
@@ -21,9 +36,14 @@ func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, 
 	return ebt, profit, ratio
 }
 
-func getUserInput(infoText string) float64 {
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
-	return userInput
+
+	if userInput <= 0 {
+		return 0, errors.New("Value must be a positive number.")
+	}
+
+	return userInput, nil
 }
